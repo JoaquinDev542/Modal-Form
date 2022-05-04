440cf4ba6501ae5beccb3b15a661f1064d13db48
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-05-04 13:16:44 +0200

    upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const $btn = document.querySelector(".btn");
+const $modal = document.querySelector(".modal");
+const $close = document.querySelector(".close");
+
+    $btn.addEventListener("click" , () => {
+        $modal.classList.toggle("active");
+    })
+
+    $close.addEventListener("click" , () => {
+        $modal.classList.remove("active" , "disable");
+    })
