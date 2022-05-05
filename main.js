32f94536ed9efbfe2723006a9ef88f972b24db5a
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-05-05 13:31:32 +0200

    add js

--- main.js
+++ main.js
@@ -2,12 +2,41 @@ const $btn = document.querySelector(".btn");
 const $modal = document.querySelector(".modal");
 const $close = document.querySelector(".close");
 
-    $btn.addEventListener("click" , () => {
-        $modal.classList.toggle("active");
-    })
+const $input = document.querySelector(".input");
+const $formBtn = document.querySelector(".form__btn");
+const regExEmail = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    $close.addEventListener("click" , () => {
-        $modal.classList.remove("active" , "disable");
-    })
+const $cardValidateMesagge = document.querySelector(".card__validateMesagge");
+const $cardErrorMesagge = document.querySelector(".card__errorMesagge");
 
-// Añadir transiciones cuando click, etc, etc
+    const eventListenerFunction = function () {
+
+        $btn.addEventListener("click" , () => {
+            $modal.classList.toggle("active");
+        })
+    
+        $close.addEventListener("click" , () => {
+            $modal.classList.remove("active" , "disable");
+        })    
+
+    }
+eventListenerFunction();
+
+    const validateEmailFunction = function() {
+
+        $formBtn.addEventListener("click" , () => {
+
+            if(regExEmail.test($input.value) === true ) {
+                $input.value = "";
+                $cardValidateMesagge.classList.add("display");
+                $cardErrorMesagge.classList.remove("display");
+
+            } else {
+                $cardValidateMesagge.classList.remove("display");
+                $cardErrorMesagge.classList.add("display");
+            }
+
+        })
+       
+    }
+validateEmailFunction();
